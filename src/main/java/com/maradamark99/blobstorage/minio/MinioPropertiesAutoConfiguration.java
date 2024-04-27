db28a0f3b2f435bb142eaf7d7ccdfe8dc4a6c940
Mark Marada <maradamark99@gmail.com>
2024-04-27 17:44:00 +0200

    Add configuration properties scan annottation

--- src/main/java/com/maradamark99/blobstorage/minio/MinioPropertiesAutoConfiguration.java
+++ src/main/java/com/maradamark99/blobstorage/minio/MinioPropertiesAutoConfiguration.java
@@ -2,21 +2,49 @@ package com.maradamark99.blobstorage.minio;
 
 import com.maradamark99.blobstorage.Constants;
 import io.minio.MinioClient;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ConfigurationProperties(prefix = Constants.ROOT_PREFIX)
+@ConditionalOnMissingBean(MinioProperties.class)
+@AutoConfiguration
+@ConfigurationProperties(prefix = Constants.ENV_ROOT_PREFIX)
+@ConfigurationPropertiesScan
 public class MinioPropertiesAutoConfiguration {
 
-    String accessKey;
+    private String accessKey;
 
-    String secretKey;
+    private String secretKey;
 
-    String endpoint;
+    private String endpoint;
+
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
 
     @Bean
     @ConditionalOnClass(MinioClient.class)
